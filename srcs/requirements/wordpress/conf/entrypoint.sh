276924b58bd655e78a623a1d547421a782d7b7ba
bducrocq <benjamin@ben3d.fr>
2023-03-13 16:48:03 +0100

    try 2

--- srcs/requirements/wordpress/conf/entrypoint.sh
+++ srcs/requirements/wordpress/conf/entrypoint.sh
@@ -13,12 +13,14 @@
 cd /var/www/html/wordpress
 if ! wp core is-installed --allow-root
 then
-	# cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+		cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php \
+&&		sed -i "s/database_name_here/$WORDPRESS_DB_NAME/" /var/www/html/wordpress/wp-config.php \
+&&		sed -i "s/username_here/$WORDPRESS_DB_USER/" /var/www/html/wordpress/wp-config.php \
+&&		sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/" /var/www/html/wordpress/wp-config.php \
+&&		sed -i "s/localhost/$WORDPRESS_DB_HOST/" /var/www/html/wordpress/wp-config.php
+
+	
 	wp core install	--allow-root \
-					--dbname=$WORDPRESS_DB_NAME \
-					--dbuser=$WORDPRESS_DB_USER \
-					--dbpass=$WORDPRESS_DB_PASSWORD \
-					--dbhost=$WORDPRESS_DB_HOST \
 					--url=$DOMAIN_NAME \
 					--title="$WP_SITE_TITLE" \
 					--admin_user=$WP_ADMIN \
