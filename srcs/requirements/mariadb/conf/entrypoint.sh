4ab10217f25b0a7486f16f93d1a8141c100ace26
bducrocq <benjamin@ben3d.fr>
2023-03-13 18:01:46 +0100

    push

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -1,7 +1,11 @@
 # Create db if dont existe
 # if [ ! -d "/var/lib/mysql/wordpress" ]
 # then 
-	service mysql start && mysql < /tmp/db.sql
+
+echo "HELLO ENTRYPOINT MARIADB"
+cat db.sql
+
+service mysql start && mysql < db.sql
 # fi
 # launch db
 /usr/bin/mysqld_safe
