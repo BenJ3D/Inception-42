f64f99c8d64fabf40bfe33cd6b4b4e45f02492ed
bducrocq <benjamin@ben3d.fr>
2023-03-13 17:30:52 +0100

    update entrypoint mariadb

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -1,5 +1,7 @@
 # Create db if dont existe
-service mysql start && mysql < /tmp/db.sql
-
+if [ ! -d "/var/lib/mysql/wordpress" ]
+then 
+	service mysql start && mysql < /tmp/db.sql
+fi
 # launch db
 /usr/bin/mysqld_safe
