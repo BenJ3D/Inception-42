70dc4d4854f38b31959db2e070272738acb2065f
bducrocq <bducrocq@bducrocq.42.fr>
2023-03-14 14:50:29 -0400

    ca re marche avant env

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # WORDPRESS SITE SETUP
+# cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php \
+# &&		sed -i "s/database_name_here/$WORDPRESS_DB_NAME/" /var/www/html/wordpress/wp-config.php \
+# &&		sed -i "s/username_here/$WORDPRESS_DB_USER/" /var/www/html/wordpress/wp-config.php \
+# &&		sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/" /var/www/html/wordpress/wp-config.php \
+# &&		sed -i "s/localhost/$WORDPRESS_DB_HOST/" /var/www/html/wordpress/wp-config.php
+
 cd /var/www/html/wordpress
+if ! wp core is-installed --allow-root
+then
 wp core install	--allow-root \
 				--url=$DOMAIN_NAME \
 				--title="$WP_SITE_TITLE" \
 				--admin_user=$WP_ADMIN \
 				--admin_password=$WP_ADMIN_PASSWORD \
-				--admin_email=$WP_ADMIN_EMAIL \
-				--skip-check 
-cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php \
-&&		sed -i "s/database_name_here/$WORDPRESS_DB_NAME/" /var/www/html/wordpress/wp-config.php \
-&&		sed -i "s/username_here/$WORDPRESS_DB_USER/" /var/www/html/wordpress/wp-config.php \
-&&		sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/" /var/www/html/wordpress/wp-config.php \
-&&		sed -i "s/localhost/$WORDPRESS_DB_HOST/" /var/www/html/wordpress/wp-config.php
-
+				--admin_email=$WP_ADMIN_EMAIL
+fi
 # LAUNCH PHP-FPM
-php-fpm7.3 -F
+php-fpm7.3 -F		
